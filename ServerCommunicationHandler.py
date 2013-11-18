19835b66972dc661ce994202c16ea948e7e05b9b
Wil <dwg4ck@virginia.edu>
2013-11-18 14:54:52 -0500

    Wil: added logging to methods that should spawn log entries, and methods to retrieve info about users and server

--- ServerCommunicationHandler.py
+++ ServerCommunicationHandler.py
@@ -27,22 +27,22 @@ class ServerCommunicationHandler(threading.Thread):
         self.start_server()
         self.account_manager = account_manager
         self.username_file_ip = dict()
-        self.log = Log
-        entry = LogEntry.__init__("Admin", "Created Server")
+        self.log = Log.Log.__init__()
+        entry = LogEntry.LogEntry.__init__("Admin", "Created Server")
         self.log.addEntry(entry)
         
     def create_new_account(self, username, password):
         #create the specified account, send back confirmation of creation
         print 'received user-id: ' + username
         print 'received password: ' + username
-        entry = LogEntry.__init__(username, "Created an account")
+        entry = LogEntry.LogEntry.__init__(username, "Created an account")
         self.log.addEntry(entry)
         self.account_manager.createAccount(username, password, "TestDirName", self.account_manager.serverDirectoryId)
 
     def sign_in(self, client_ip, client_port, username, user_password):
         
         if self.account_manager.loginAccount(username, user_password): #if the login was successful
-            entry = LogEntry.__init__(username, "Logged In")
+            entry = LogEntry.LogEntry.__init__(username, "Logged In")
             self.log.addEntry(entry)
             if username in self.clients:#if the same username has already logged in from other ip/port
                 print "User " + username + " has logged in from other IP address, but hey you can still join using this IP!"
@@ -75,6 +75,8 @@ class ServerCommunicationHandler(threading.Thread):
 
     def receive_file(self, filename, filedata, username, source_ip, source_port):
         if self.check_sign_in(username, source_ip, source_port): #if the client (IP and Port) has signed in
+            entry = LogEntry.LogEntry.__init__("Server", "Received File: " + filename + " from " + username )
+            self.log.addEntry(entry)
             path, name = os.path.split(filename)
             print "Path: " + path
             print "Name: " + name
@@ -93,6 +95,8 @@ class ServerCommunicationHandler(threading.Thread):
 
     def receive_folder(self, folder_name, username, source_ip, source_port):
         if self.check_sign_in(username, source_ip, source_port): #if the client (IP and Port) has signed in
+            entry = LogEntry.LogEntry.__init__("Server", "Received Folder: " + folder_name + " from " + username )
+            self.log.addEntry(entry)
             user_root_dir = self.account_manager.getAccountDirectory(username)
             print "user_root = " + `user_root_dir`
             if not os.path.exists(user_root_dir + folder_name):
@@ -104,6 +108,8 @@ class ServerCommunicationHandler(threading.Thread):
         #send a file to be copied to the local  machine
         # authenticate user
         if self.check_sign_in(username, client_ip, client_port): # if signed in
+            entry = LogEntry.LogEntry.__init__("Server", "Sent File: " + filename + " to " + username )
+            self.log.addEntry(entry)
             with open(self.account_manager.getAccountDirectory(username) + filename, "rb") as handle:
                 binary_data = xmlrpclib.Binary(handle.read())
                 print "File " + filename + "sent to " + client_ip
@@ -145,7 +151,7 @@ class ServerCommunicationHandler(threading.Thread):
         print "System has " + num_files + " files totaling " + size_files + " bits between " + num_users + " users"
 
     def print_log(self):
-        self.log.print_log()
+        self.log.log.print_log()
 
 
     def start_server(self):
@@ -154,7 +160,7 @@ class ServerCommunicationHandler(threading.Thread):
         self.server.register_introspection_functions()
         server_wait = threading.Thread(target=self.server.serve_forever)
         server_wait.start()
-        entry = LogEntry.__init__("Admin", "Started Server")
+        entry = LogEntry.LogEntry.__init__("Admin", "Started Server")
         self.log.addEntry(entry)
         print "server activated, server alive: " + str(server_wait.isAlive()) + ". Server IP: " + self.ip
 
