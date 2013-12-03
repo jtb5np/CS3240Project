2a0ac655914213dd1d6dba9b2c87be78cc800e29
Jacob <jtb5np@virginia.edu>
2013-12-02 21:59:37 -0500

    Finished file sharing and cleaned up sync process a little bit.

--- Client/SyncProcess.py
+++ Client/SyncProcess.py
@@ -11,30 +11,6 @@ import sys
 import socket
 
 
-def listen_for_connection(ch):
-    l = multiprocessing.connection.Listener(address=('localhost', 6000))
-    connection = l.accept()
-    while True:
-        try:
-            received = connection.recv()
-        except EOFError:
-            received = ''
-            connection.close()
-            connection = l.accept()
-        if received == 'on':
-            ch.sync_on = True
-        elif received == 'off':
-            ch.sync_on = False
-        elif received == 'create':
-            user_id = connection.recv()
-            password = connection.recv()
-            response = ch.create_new_account(user_id, password)
-            connection.send(response)
-        elif received == 'change':
-            password = connection.recv()
-            response = ch.change_password(password)
-            connection.send(response)
-
 def main():
     answer = ''
     if os.path.exists('account_info.txt'):
@@ -42,14 +18,12 @@ def main():
         info_file = open('account_info.txt', 'r')
         user_id = info_file.readline().rstrip('\n')
         password = info_file.readline().rstrip('\n')
-        #root_folder = info_file.readline().rstrip('\n')
         info_file.close()
     else:
         while not (answer == '1' or answer == '2'):
             answer = raw_input('Type 1 to create new account' + '\n' + 'Type 2 to sign in to existing account')
         user_id = raw_input('Enter username: ')
         password = raw_input('Enter password: ')
-        #root_folder = raw_input("Enter the name of the directory you want to synchronize: ")
     root_folder = 'onedir'
     try:
         os.mkdir(root_folder)
@@ -60,17 +34,17 @@ def main():
     files_to_delete = Queue()
     files_to_receive = Queue()
     deleted_files_to_receive = Queue()
-    fwr = FileWatcher.FileWatcher(files_to_send, files_to_delete, files_to_receive, deleted_files_to_receive, root_folder)
+    fwr = FileWatcher.FileWatcher(files_to_send, files_to_delete, files_to_receive, deleted_files_to_receive,
+                                  root_folder)
 
     local_port = 9000
-
-    #test script
     server_ip = raw_input('Enter server IP address: ')
     server_port = 8000
     local_ip = get_local_ip()
 
-    lch = LocalCommunicationHandler.LocalCommunicationHandler(server_ip, server_port, local_ip, local_port, root_folder, files_to_send, files_to_delete, files_to_receive, deleted_files_to_receive)
-    listener_thread = threading.Thread(target=listen_for_connection, args=(lch,))
+    lch = LocalCommunicationHandler.LocalCommunicationHandler(server_ip, server_port, local_ip, local_port, root_folder,
+                                                              files_to_send, files_to_delete, files_to_receive,
+                                                              deleted_files_to_receive)
 
     fwr.start()
     signed_in = False
@@ -92,17 +66,19 @@ def main():
 
     if signed_in:
         lch.start()
-        listener_thread.start()
     else:
         print "Exiting..."
-        exit_client(lch, fwr, listener_thread)
+        exit_client(lch, fwr)
 
 
     main_menu = "1. Change Password\n2. Turn off synchronization\n3. Share files\n4. Sign Out and Exit"
     exit = False
     while not exit:
         print main_menu
-        selection = int(raw_input("Please choose from the menu: "))
+        try:
+            selection = int(raw_input("Please choose from the menu: "))
+        except ValueError:
+            continue
         if selection == 1:
             password = str(raw_input("Input your new password: "))
             if lch.change_password(password):
@@ -115,7 +91,7 @@ def main():
             if lch.sign_out():
                 print "Sign out successful"
                 signed_back = False
-                while signed_back == False:
+                while not signed_back:
                     sign_back = raw_input("Would you like to sign back in? y/n")
                     if sign_back == 'y':
                         if lch.sign_in(user_id, password):
@@ -150,13 +126,14 @@ def main():
             continue
 
     # Exiting procedure
-    exit_client(lch, fwr, listener_thread)
+    exit_client(lch, fwr)
 
 
-def exit_client(lch = None, fwr = None, listener_thread = None):
-    if lch is not None: lch._Thread__stop()
-    if fwr is not None: fwr._Thread__stop()
-    if listener_thread is not None: listener_thread._Thread__stop()
+def exit_client(lch = None, fwr = None):
+    if lch is not None:
+        lch._Thread__stop()
+    if fwr is not None:
+        fwr._Thread__stop()
     print "Exited."
     sys.exit(0)
 
@@ -168,5 +145,5 @@ def get_local_ip():
     s.close()
     return current_local_ip
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
